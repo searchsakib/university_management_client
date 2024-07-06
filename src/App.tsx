0b92811a7a9dae72f905be6eb184a38cb4db61a9
searchsakib <searchsakib@gmail.com>
2024-07-06 16:26:53 +0600

    basic ant design sidebar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import MainLayout from './components/layout/MainLayout';
+
 function App() {
   return (
     <>
       <h2>University Management App</h2>
+      <MainLayout></MainLayout>
     </>
   );
 }
